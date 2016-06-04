37361318bc293eda24ccd01cb3844e17125f0d3d
thienvumonitor09 <thienvumonitor09@gmail.com>
2016-06-04 11:21:16 -0700

    Delete MapSite.java

--- MapSite.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*; 
-
-public interface MapSite extends Cloneable { 
-
-  public Object clone() throws CloneNotSupportedException;
-  public void enter(Maze maze); 
-  public void draw(Graphics g, int x, int y, int w, int h); 
-
-}
