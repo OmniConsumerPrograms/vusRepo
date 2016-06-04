4e26cbdb57efdfae03f19f2e4872d8ac8791f0eb
thienvumonitor09 <thienvumonitor09@gmail.com>
2016-06-04 11:28:39 -0700

    Add files via upload

---
+++ Position.java
@@ -0,0 +1,18 @@
+
+
+public class Position{
+	public int x;     //row
+	public int y;     //column
+	//public char val;  //1, 0, or 'X'
+	
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+		//this.val = val;
+	}
+	
+	
+	
+	
+	
+}
